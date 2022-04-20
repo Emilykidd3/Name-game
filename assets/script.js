01f86520584a59132b853d380f0e608be714c6e0
emilykidd3 <e.kidd61@yahoo.com>
2022-04-19 21:53:13 -0700

    updatd results for null cases, and added function to log likely gender

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,11 @@ const searchButton = document.querySelector("#search-button");
 const nameInput = document.querySelector("#name-input");
 
 function search() {
+    findAge();
+    findGender();
+}
+
+function findAge() {
     let name = nameInput.value.trim();
     fetch(`https://api.agify.io?name=${name}&country_id=US`)
     .then(function(response) {
@@ -15,10 +20,28 @@ function search() {
     })
 }
 
+function findGender() {
+    let name = nameInput.value.trim();
+    fetch(`https://api.genderize.io?name=${name}&country_id=US`)
+    .then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data, name);
+            });
+        } else {
+            console.error("Error: "+response.statusText);
+        }
+    })
+}
+
 function displayResults(data, name) {
     const resultsDiv = document.querySelector("#results")
-    resultsDiv.textContent = `The average age of some named ${name} is ${data.age}`;
-    console.log(data.age)
+    if (data.age === null) {
+        resultsDiv.textContent = `There are no results for someone named ${name}, try another name!`;
+    } else {
+        resultsDiv.textContent = `The predicted age of someone named ${name} is ${data.age}`;
+    }
+    console.log(data)
 }
 
 searchButton.addEventListener("click", search);
